51b0836885e3f31eefc66e799a15fbfe8d5563f3
Thomas Couchoud <zerderr@gmail.com>
2019-01-07 15:16:35 +0100

    Add base project

---
+++ TP2/Java/src/main/java/fr/mrcraftcod/reconnaissancesdeformes/Main.java
@@ -0,0 +1,18 @@
+package fr.mrcraftcod.reconnaissancesdeformes;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by Thomas Couchoud (MrCraftCod - zerderr@gmail.com) on 2018-12-21.
+ *
+ * @author Thomas Couchoud
+ * @since 2018-12-21
+ */
+public class Main{
+	private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
+	
+	public static void main(String[] args){
+		LOGGER.info("Hello");
+	}
+}
